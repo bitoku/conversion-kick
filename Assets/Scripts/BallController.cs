d35639a05a77069c33610585e016efac5b6e3cd3
Ayato Tokubi <tokubi.ayato@gmail.com>
2020-08-09 20:15:32 +0900

    add script

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -7,6 +7,9 @@ public class BallController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Vector3 initialDir = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        float power = Random.Range(10f, 20f);
+        
         
     }
 
